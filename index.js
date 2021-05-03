dd8aeef96fbc7132b721a657a0b9627ebdb6bafd
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-03 15:15:29 -0500

    added CRUD

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const { createClient } = require("@supabase/supabase-js");
-
-// Create a single supabase client for interacting with your database
 const supabase = createClient(
   "https://trvnebvemlsnomuzdgff.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDA2MTU1NCwiZXhwIjoxOTM1NjM3NTU0fQ.J892pC3M_hmQCZ91fkQoo-iUXuOoQuNrYzZ7la_idIk"
@@ -10,30 +8,43 @@ const supabase = createClient(
 
 const PORT = 3000;
 
+// creates entry into "Parks" method using data supplied. Can pull this data from a form on site.
 app.post("/create", async (req, res) => {
-  const { data, error } = await supabase.from("Users").insert([
+  const { data, error } = await supabase.from("Parks").insert([
     {
-      firstName: "Bob",
-      lastName: "Bobberson",
-      email: "bobby@gmail.com",
-      username: "bob1234",
-      password: "urmom",
+      parkName: "Mojave Desert",
+      directionsURL: "www.mojave.com",
+      moreInfoURL: "wwww.moremojaveinfo.com",
+      userId: 3,
     },
   ]);
   res.send("WORKED!");
 });
 
-app.post("/create", async (req, res) => {
-  const { data, error } = await supabase.from("Users").insert([
-    {
-      firstName: "Bob",
-      lastName: "Bobberson",
-      email: "bobby@gmail.com",
-      username: "bob1234",
-      password: "urmom",
-    },
-  ]);
-  res.send("WORKED!");
+// Pulls all data from "Users" table. Console logging to ensure data is pulled correctly.
+app.get("/getdata", async (req, res) => {
+  const { data, error } = await supabase.from("Users").select();
+  console.log(data);
+  res.send("GOT THE DATA!");
+});
+
+// finds User named "Bob" and updates their name to "New Zealand". Can match with any of the column names (id, lastName, etc).
+app.post("/updatedata", async (req, res) => {
+  const { data, error } = await supabase
+    .from("Users")
+    .update({ firstName: "New Zealand" })
+    .match({ firstName: "Bob" });
+  console.log(data);
+  res.send("CHANGED!");
+});
+
+// deletes data from "Parks" table with the name of "Yellowstone"
+app.post("/deletedata", async (req, res) => {
+  const { data, error } = await supabase
+    .from("Parks")
+    .delete()
+    .match({ parkName: "Yellowstone" });
+  res.send("GONE!");
 });
 
 app.listen(PORT, () => {
