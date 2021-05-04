58787a58ce10e2757841104faf36a3e770bf6196
Patrick Groves <patrickalexgroves@gmail.com>
2021-05-04 13:28:29 -0600

    pooosh

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ app.use(
 
 
 
-app.use(passport.initialize())
-app.use(passport.session())
-
 const path = require("path");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
@@ -24,9 +21,14 @@ const supabase = createClient(
   "https://trvnebvemlsnomuzdgff.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDA2MTU1NCwiZXhwIjoxOTM1NjM3NTU0fQ.J892pC3M_hmQCZ91fkQoo-iUXuOoQuNrYzZ7la_idIk"
 );
+app.use(session({
+  secret: 'aksdjfj33',
+  resave: false,
+  saveUninitialized: false
+}))
 
 const PORT = 3000;
-const SALT_ROUNDS = 10;
+
 
 // // creates entry into "Parks" method using data supplied. Can pull this data from a form on site.
 // app.post("/create", async (req, res) => {
@@ -42,17 +44,15 @@ const SALT_ROUNDS = 10;
 // });
 
 app.get('/register', (req,res) => {
+  console.log(req.body)
   res.render('register')
 })
 
-app.post('/register', async (req,res) => {
-
-  const { firstName, lastName, email, username, password } = req.body
-
-  const hash = bcrypt.hash(password,SALT_ROUNDS, function(error,hash) {
-    if(error == null){return hash}})
-
-    const { data, error } = await supabase.from("Users").insert([
+app.post("/register", async (req, res) => {
+  const { firstName, lastName, email, username, password } = req.body;
+  const SALT = await bcrypt.genSalt();
+  const hash = await bcrypt.hash(req.body.password, SALT);
+  const { data, error } = await supabase.from("Users").insert([
     {
       firstName: firstName,
       lastName: lastName,
@@ -61,9 +61,9 @@ app.post('/register', async (req,res) => {
       password: hash,
     },
   ]);
-  console.log("REGISTERED")
-  res.redirect('explore')
-})
+  console.log("REGISTERED");
+  res.redirect("explore");
+});
 
 app.get('/login', (req,res) => {
   res.render('login')
@@ -71,26 +71,31 @@ app.get('/login', (req,res) => {
 app.post("/login", async (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
-  const { user, error } = await supabase
+  const { data, error } = await supabase
     .from("Users")
     .select()
     .match({ username: username })
-  if (user != null) {
-    console.log("not null")
-    bcrypt.compare(password, user.password, (error, result) => {
+  if (data != null) {
+    console.log(data)
+    bcrypt.compare(password, data.password, async (error, result) => {
+      console.log(result)
       if (result) {
-        if (req.session) {
-          req.session.user = { userId: user.id };
-          res.redirect("explore");
-        }
-      } else {
-        res.send("no result");
+        console.log(result)
+      //   if (req.session) {
+      //     res.redirect('explore');
+      //   }
+      // } else {
+      //   res.redirect('login');
       }
-    });
+    })
   } else {
     res.render("login", { message: "Incorrect Username or Password" });
   }
 });
+
+app.get('/explore', (req,res) => {
+  res.render('explore')
+})
 
 // Pulls all data from "Users" table. Console logging to ensure data is pulled correctly.
 // app.get("/getdata", async (req, res) => {
