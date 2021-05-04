c3b94d1cfdc534ef53202a3df9f60b191b3b7074
Patrick Groves <patrickalexgroves@gmail.com>
2021-05-04 10:12:47 -0600

    register and hashword features

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 const es6Renderer = require("express-es6-template-engine");
 app.engine("html", es6Renderer);
-// app.set("views", "template"); // will set when there is a folder to connect to
-// app.set("view engine", "html"); // will set when there is a folder to connect to
+app.set("views", "client"); // will set when there is a folder to connect to
+app.set("view engine", "html"); // will set when there is a folder to connect to
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+
+
 const path = require("path");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
@@ -22,44 +24,68 @@ const supabase = createClient(
 const PORT = 3000;
 const SALT_ROUNDS = 10;
 
-// creates entry into "Parks" method using data supplied. Can pull this data from a form on site.
-app.post("/create", async (req, res) => {
-  const { data, error } = await supabase.from("Parks").insert([
+// // creates entry into "Parks" method using data supplied. Can pull this data from a form on site.
+// app.post("/create", async (req, res) => {
+//   const { data, error } = await supabase.from("Parks").insert([
+//     {
+//       parkName: "Redwood Forest",
+//       directionsURL: "www.redwoods.com",
+//       moreInfoURL: "wwww.moreredwoodinfo.com",
+//       userId: 4,
+//     },
+//   ]);
+//   res.send("WORKED!");
+// });
+
+app.get('/register', (req,res) => {
+  res.render('register')
+})
+
+app.post('/register', async (req,res) => {
+
+  const { firstName, lastName, email, username, password } = req.body
+
+  const hash = bcrypt.hash(password,SALT_ROUNDS, function(error,hash) {
+    if(error == null){return hash}})
+
+    const { data, error } = await supabase.from("Users").insert([
     {
-      parkName: "Redwood Forest",
-      directionsURL: "www.redwoods.com",
-      moreInfoURL: "wwww.moreredwoodinfo.com",
-      userId: 4,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      username: username,
+      password: hash,
     },
   ]);
-  res.send("WORKED!");
-});
+  console.log("BACKEND INSERTION")
+  res.render('explore')
+})
 
 // Pulls all data from "Users" table. Console logging to ensure data is pulled correctly.
-app.get("/getdata", async (req, res) => {
-  const { data, error } = await supabase.from("Users").select();
-  console.log(data);
-  res.send("GOT THE DATA!");
-});
+// app.get("/getdata", async (req, res) => {
+//   const { data, error } = await supabase.from("Users").select();
+//   console.log(data);
+//   res.send("GOT THE DATA!");
+// });
 
-// finds User named "Bob" and updates their name to "New Zealand". Can match with any of the column names (id, lastName, etc).
-app.post("/updatedata", async (req, res) => {
-  const { data, error } = await supabase
-    .from("Users")
-    .update({ firstName: "New Zealand" })
-    .match({ firstName: "Bob" });
-  console.log(data);
-  res.send("CHANGED!");
-});
+// // finds User named "Bob" and updates their name to "New Zealand". Can match with any of the column names (id, lastName, etc).
+// app.post("/updatedata", async (req, res) => {
+//   const { data, error } = await supabase
+//     .from("Users")
+//     .update({ firstName: "New Zealand" })
+//     .match({ firstName: "Bob" });
+//   console.log(data);
+//   res.send("CHANGED!");
+// });
 
-// deletes data from "Parks" table with the name of "Yellowstone"
-app.post("/deletedata", async (req, res) => {
-  const { data, error } = await supabase
-    .from("Parks")
-    .delete()
-    .match({ parkName: "Yellowstone" });
-  res.send("GONE!");
-});
+// // deletes data from "Parks" table with the name of "Yellowstone"
+// app.post("/deletedata", async (req, res) => {
+//   const { data, error } = await supabase
+//     .from("Parks")
+//     .delete()
+//     .match({ parkName: "Yellowstone" });
+//   res.send("GONE!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on localhost:${PORT}`);
