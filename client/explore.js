6ec134f3b3615ab878d61b1b7724dd8c4fa3245e
Jessicahaugen <j.rose.haugen@gmail.com>
2021-05-04 21:36:30 -0700

    refactored function

--- client/explore.js
+++ client/explore.js
@@ -12,19 +12,16 @@ const statesByRegion = {
 }
 
 
-const getParkByRegion = async (region) => {
-    const states = statesByRegion[region]
-    console.log(region)
+const getParkByState = async (state) => {
     const getInfo = await fetch(
-    `https://developer.nps.gov/api/v1/parks?stateCode=${states.join(",")},ID&api_key=fulAEa7kmQxxruUH93NX1dJp9KT8W7O1loEFHwly`
+    `https://developer.nps.gov/api/v1/parks?stateCode=${state}&api_key=fulAEa7kmQxxruUH93NX1dJp9KT8W7O1loEFHwly`
     );
     const convertInfo = await getInfo.json();
     // convertInfo.data = convertInfo.data
     // .filter((d) => {
     //   if (camping) {
-    //     return d.activities.find((a) => a.name === camping);
+    //     return d.activities.find((a) => a.name === "camping");
     //   }
-    //     true
     // }); 
     parkList.innerHTML = "";
 
@@ -43,8 +40,17 @@ const getParkByRegion = async (region) => {
         infoDiv.className="infoDiv";
         let parkState = document.createElement("h3");
         parkState.innerHTML = `State: ${convertInfo.data[i].states}`;
-        let parkInfo = document.createElement("p");
-        parkInfo.innerText = convertInfo.data[i].description;
+        let viewMore = document.createElement("button");
+        viewMore.className = "moreInfo"
+        viewMore.innerHTML = "More Info?"
+        viewMore.addEventListener("click",function(){
+            let parkInfo = document.createElement("p");
+            parkInfo.innerText = convertInfo.data[i].description;
+            let parkDirections = document.createElement("div");
+            parkDirections.innerHTML = convertInfo.data[i].directionsInfo + convertInfo.data[i].directionsUrl;
+            
+            infoDiv.append(parkInfo,parkDirections)
+        })
         let parkName = document.createElement("h2");
         parkName.className= "parkNames";
         parkName.innerHTML = convertInfo.data[i].fullName;
@@ -52,29 +58,51 @@ const getParkByRegion = async (region) => {
         itenerary.innerHTML = `add to itenerary <i class="far fa-heart"></i>`;
         itenerary.className = "addProfile";
         
-        itenerary.addEventListener("click", function () {
-          favorites.push(convertInfo.data[i].id);
-          itenerary.className += "add";
-          alert("This park has been added to your itenerary! Good luck on your Explorations!");
-        })
-      infoDiv.append(parkName, parkState, parkInfo, itenerary);
+        
+        // itenerary.addEventListener("click", function () {
+        //   (THIS WILL BE WHERE DATABASEINFO TO ADD TO ITENERARY).push(convertInfo.data[i].id);
+        //   itenerary.className += "add";
+        //   alert("This park has been added to your itenerary! Good luck on your Explorations!");
+        // })
+      infoDiv.append(parkName, parkState, viewMore, itenerary);
       imgDiv.append(mainImg);
       parkContainer.append(imgDiv, infoDiv);
       parkList.append(parkContainer);
     }
 };
-    
-let selector = document.querySelector(".selectpicker");
-selector.addEventListener("change", (e) => {  
-    const region = e.target.value;
-    getParkByRegion(region);
 
+let form = document.querySelector("form.fullSearch");
+let regionSelector = form.querySelector(".selectpicker");
+let stateSelector = form.querySelector(".selectState");
+let camp = form.querySelector("#camping");
+
+regionSelector.addEventListener("change", (e) => {  
+    const region = e.target.value;
+    const states = statesByRegion[region];
+
+    stateSelector.innerHTML = `<option value=""> States</option>`;
+    states.forEach(state => {
+        stateSelector.innerHTML += "<option value='" + state + "'>" + state + "</option>";
+    });
 });
 
-// let camp = document.querySelector("#camping");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const state = stateSelector.value;
+    getParkByState(state,camping);
+
+    //possibly where we send date data
+    // const date = form.querySelector( ... ).value;
+    // fetch(...) // send the request to the BE with the dates
+
+    return false;
+});
+
+//camping option
 // camp.addEventListener("click", (e) => {  
-//     // const region = e.target.value;
-//     getParkByRegion(camping);
+//     getParkByRegion(state,camping);
+//     console.log(camp)
 
 // });
 
